32176ca4790490d85aa1fda188544a2c96d4cff3
Carla Miles <ms.carla.albert@gmail.com>
2019-11-10 19:23:28 -0600

    integrated results from Google Books API into class instances and created functionality to save books from separate queries into one reading list

--- book_search/__main__.py
+++ book_search/__main__.py
@@ -1,6 +1,5 @@
 import sys, requests
 from .classmodule import Book, ReadingList
-from .funcmodule import my_function
 from os import getenv
 from dotenv import load_dotenv, find_dotenv
 
@@ -20,17 +19,20 @@ def main():
     # my_list = ReadingList()
     # my_list.add_book(my_first_book).add_book(my_second_book).add_book(my_third_book)
     # my_list.display_reading_list()
-    url = 'https://www.googleapis.com/books/v1/volumes?q=' 
-    args = sys.argv[1:]
-    for arg in args:
-        query = arg + '+' 
-    # flowers
-    result_num = '&maxResults=5'
-    key = '&key=' + getenv('api_key')
 
-    api_call = url + query + result_num + key
-    print(api_call)
-    json_data = requests.get(api_call).json()
+    # args = sys.argv[1:]
+    # for arg in args:
+    #     query = arg + '+' 
+    
+    # search = input('Enter your book search query: ')
+    
+    # url = 'https://www.googleapis.com/books/v1/volumes?q=' 
+    # result_num = '&maxResults=5'
+    # key = '&key=' + getenv('api_key')
+
+    # api_call = url + search + result_num + key
+    # print(api_call)
+    # json_data = requests.get(api_call).json()
 
     # for i in range(5):
     #     print('Authors:', json_data['items'][i]['volumeInfo']['authors'])
@@ -40,19 +42,40 @@ def main():
     # for i in range(5):
     #     result_[i] = Book(json_data['items'][i]['volumeInfo']['authors'], json_data['items'][i]['volumeInfo']['title'], json_data['items'][i]['volumeInfo']['publisher'])
 
-    book_1 = Book(json_data['items'][0]['volumeInfo']['authors'], json_data['items'][0]['volumeInfo']['title'], json_data['items'][0]['volumeInfo']['publisher'])
-    book_2 = Book(json_data['items'][1]['volumeInfo']['authors'], json_data['items'][1]['volumeInfo']['title'], json_data['items'][1]['volumeInfo']['publisher'])
-    book_3 = Book(json_data['items'][2]['volumeInfo']['authors'], json_data['items'][2]['volumeInfo']['title'], json_data['items'][2]['volumeInfo']['publisher'])
-    book_4 = Book(json_data['items'][3]['volumeInfo']['authors'], json_data['items'][3]['volumeInfo']['title'], json_data['items'][3]['volumeInfo']['publisher'])
-    book_5 = Book(json_data['items'][4]['volumeInfo']['authors'], json_data['items'][4]['volumeInfo']['title'], json_data['items'][4]['volumeInfo']['publisher'])
+    # book_1 = Book(json_data['items'][0]['volumeInfo']['authors'], json_data['items'][0]['volumeInfo']['title'], json_data['items'][0]['volumeInfo']['publisher'])
+    # book_2 = Book(json_data['items'][1]['volumeInfo']['authors'], json_data['items'][1]['volumeInfo']['title'], json_data['items'][1]['volumeInfo']['publisher'])
+    # book_3 = Book(json_data['items'][2]['volumeInfo']['authors'], json_data['items'][2]['volumeInfo']['title'], json_data['items'][2]['volumeInfo']['publisher'])
+    # book_4 = Book(json_data['items'][3]['volumeInfo']['authors'], json_data['items'][3]['volumeInfo']['title'], json_data['items'][3]['volumeInfo']['publisher'])
+    # book_5 = Book(json_data['items'][4]['volumeInfo']['authors'], json_data['items'][4]['volumeInfo']['title'], json_data['items'][4]['volumeInfo']['publisher'])
 
-    print(book_1.say_title())
-    print(book_2.say_author())
-    print(book_3.say_publisher())
+    # print('book_1 = ', book_1.say_title())
+    # print('book_2 = ', book_2.say_author())
+    # print('book_3 = ', book_3.say_publisher())
 
     my_list = ReadingList()
-    my_list.add_book(book_1).add_book(book_2).add_book(book_5)
+
+    for t in range(3):
+        search = input('Enter your book search query: ')
+    
+        url = 'https://www.googleapis.com/books/v1/volumes?q=' 
+        result_num = '&maxResults=5'
+        key = '&key=' + getenv('api_key')
+
+        api_call = url + search + result_num + key
+        # print(api_call)
+        json_data = requests.get(api_call).json()
+
+        objs = [Book(json_data['items'][i]['volumeInfo']['authors'], json_data['items'][i]['volumeInfo']['title'], json_data['items'][i]['volumeInfo']['publisher']) for i in range(5)]
+        for obj in objs:
+            print('*'*50)
+            print('Author(s): ', obj.author)
+            print('Title: ', obj.title)
+            print('Publisher: ', obj.publisher)
+            print('*'*50)
+        my_list.add_book(objs[int(input('Enter the number of the book you want to add to your reading list (in the order it appears in your results): ')) - 1])
+        
     print('The following is on your reading list:')
     my_list.display_reading_list()
+
 if __name__ == '__main__':
     main()
--- book_search/classmodule.py
+++ book_search/classmodule.py
@@ -4,29 +4,31 @@ class Book():
         self.title = title
         self.publisher = publisher
     def say_author(self):
-        print('author is {}'.format(self.author))
+        print('Author: {}'.format(self.author))
         return self
     def say_title(self):
-        print('title is {}'.format(self.title))
+        print('Title: {}'.format(self.title))
         return self
     def say_publisher(self):
-        print('publisher is {}'.format(self.publisher))
+        print('Publisher: {}'.format(self.publisher))
         return self
 
 class ReadingList():
     def __init__(self):
         self.reading_list = []
-        self.book = Book("author", "title", "publisher")
+        
     def add_book(self, book):
         self.reading_list.append(book)
-        print('Added book to reading list')
+        print('Added book to reading list:', book.title, 'by', book.author, 'published by', book.publisher)
         return self
     def display_reading_list(self):
         print(self.reading_list)
         for book in self.reading_list:
+            print('*'*50)
             book.say_author()
             book.say_title()
             book.say_publisher()
+            print('*'*50)
         return self
 
     
